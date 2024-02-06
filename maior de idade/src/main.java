1a08f6df161357be5979fcac16d428f3e874be0e
renancardoso1414@gmail.com <232.933594@senaicimatec.edu.br>
2024-02-06 14:45:51 -0300

    maiordeidade

---
+++ maior de idade/src/main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class idade{
+public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int maiordeidade = 0;
+
+    System.out.println("insira sua idade: ");
+    maiordeidade = scanner.nextInt();
+    
+    if(maiordeidade >= 18){
+    System.out.println("maior de idade: ");
+    }
+    
+    if(maiordeidade <= 18){
+    System.out.println("menor de idade");    
+
+    }
+ 
+
+
+
+}
+
+
