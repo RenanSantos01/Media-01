09c3a59d1018ce22ea1d4b0ab9d2c5041db61c09
RENAN CARDOSO PORTELA DOS SANTOS <232.933594@senaicimatec.edu.br>
2024-02-06 14:15:42 -0300

    Media

---
+++ Media/src/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class media{
+public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    double somaDasnotas = 0;
+
+    for(int i = 1; i<= 3; i++){
+        System.out.println("Digite a nota do aluno" + i + ":");
+        double nota = scanner.nextDouble();
+        somaDasnotas += nota;
+
+    }
+    double media = somaDasnotas / 3;
+    System.out.println("A media das notas é: " + media);
+
+}
+
+
+
+}
+
+
